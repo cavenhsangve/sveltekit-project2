921129b9c04c742909559e49e95727ffb9fe635f
cavenhsangve <jadencaven@gmail.com>
2023-11-25 13:29:46 +1100

    Initial commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,18 @@
+<script>
+    import "../app.css";
+  </script>
+  
+<slot />
+
+<style>
+    :global(body) {
+        padding: 2rem;
+        background-color: rgb(0, 35, 48);
+        font-family: Arial, Helvetica, sans-serif;
+    }
+
+    :global(input, button) {
+        border-radius: 1rem;
+    }
+
+</style>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,9 @@
+<h1>Forms</h1>
+
+<style lang="postcss">
+    h1 {
+        font-weight: bolder;
+        font-size: xx-large;
+        color: aliceblue;
+    }
+</style>
