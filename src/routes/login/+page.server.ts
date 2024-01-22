72356a81c9a80247328fe73ffa56be5fa33868da
cavenhsangve <jadencaven@gmail.com>
2024-01-22 16:04:32 +1100

    form validation

--- src/routes/login/+page.server.ts
+++ src/routes/login/+page.server.ts
@@ -1,5 +1,25 @@
-import type { PageServerLoad } from './$types';
+import { redirect, type Actions, fail } from '@sveltejs/kit';
+import { zfd } from 'zod-form-data'
 
-export const load = (async () => {
-    return {};
-}) satisfies PageServerLoad;
+export const actions: Actions = {
+    default: async ({ request }) => {
+        const formData = await request.formData()
+        
+        const loginSchema = zfd.formData({
+            user: zfd.text(),
+            password: zfd.text()
+        })
+        const result = loginSchema.safeParse(formData)
+
+        if (!result.success) {
+            const data = {
+                data: Object.fromEntries(formData),
+                errors: result.error.flatten().fieldErrors
+            }
+
+            return fail(400, data)
+        }
+
+        throw redirect(303, '/todos')
+    }
+}
--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -1,5 +1,41 @@
 <script lang="ts">
-    import type { PageData } from './$types';
+	import { enhance } from '$app/forms';
+    import type { ActionData } from './$types';
     
-    export let data: PageData;
+    export let form: ActionData;
 </script>
+
+<pre>
+    {JSON.stringify(form, null, 2)}
+</pre>
+
+<form method="post" class="mt-10 space-y-4" use:enhance>
+    <input 
+        class="bg-inherit appearance-none border border-gray-200 rounded-lg w-full py-2 px-2 text-gray-300 leading-tight focus:border-2 focus:outline-none" 
+        type="text" 
+        name="user"
+        value={form?.data?.user ?? ''} 
+    />
+    {#if form?.errors?.user}
+        <p class="error">Name is required</p>
+    {/if}
+    
+    <input 
+        class="bg-inherit appearance-none border border-gray-200 rounded-lg w-full py-2 px-2 text-gray-300 leading-tight focus:border-2 focus:outline-none" 
+        type="password" 
+        name="password"
+    />
+    {#if form?.errors?.password}
+        <p class="error">Password is required</p>
+    {/if}
+
+    <button class="shadow bg-blue-500 hover:bg-blue-400 focus:shadow-outline focus:outline-none font-bold py-2 px-4 rounded-lg w-full" type="submit">
+        Login
+    </button>
+</form>
+
+<style>
+    .error {
+        color: tomato;
+    }
+</style>
